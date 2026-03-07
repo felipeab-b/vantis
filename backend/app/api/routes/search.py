2e6da00dff7fdc4037097aaf4e0640b087df1192
felipeab-b <felipeabferreira22@gmail.com>
2026-03-07 11:06:33 -0300

    feat(back): chamando o service vt no controller

--- backend/app/api/routes/search.py
+++ backend/app/api/routes/search.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException
 from app.services.ipinfo import get_ip_info
 from app.services.shodan import get_host_info
+from app.services.virustotal import get_ip_report
 
 router = APIRouter()
 
@@ -9,11 +10,13 @@ async def search(query: str):
     try:
         ip_data = await get_ip_info(query)
         shodan_data = await get_host_info(query)
+        vt_data = await get_ip_report(query)
 
         return {
             "query": query, 
             "ip_info": ip_data,
-            "shodan": shodan_data
+            "shodan": shodan_data,
+            "virustotal": vt_data
             }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
