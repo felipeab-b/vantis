e79d40fdd3e1f1bfe65bddd371dc0908fcb43045
felipeab-b <felipeabferreira22@gmail.com>
2026-03-07 00:09:00 -0300

    feat(mongo): vinculando o ciclo de vida do mongo com o inicio e fim da aplicação

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from app.db.mongo import connect_db, close_db
 
 app = FastAPI(
     title="Vantis API",
     description="Attack surface intelligence platform",
     version="0.1.0"
 )
+
+@app.on_event("startup")
+async def startup():
+    await connect_db()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await close_db()
 
 @app.get("/")
 def root():
