70ddae3f172feb78187bbbd50cb297976b9ed48f
felipeab-b <felipeabferreira22@gmail.com>
2026-03-07 00:03:31 -0300

    feat(mongo): função de conexão com o database

---
+++ backend/app/db/mongo.py
@@ -0,0 +1,12 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from app.config import settings
+
+client: AsyncIOMotorClient = None
+db = None
+
+async def connect_db():
+    global client, db
+    client = AsyncIOMotorClient(settings.MONGODB_URL)
+    db = client[settings.DATABASE_NAME]
+    print("Connected to MongoDB")
+
