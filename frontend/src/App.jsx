5ac470b7f2b9a210b64e1ff7c6315a14274b855f
felipeab-b <felipeabferreira22@gmail.com>
2026-03-08 01:54:43 -0300

    feat(front): inicio home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Report from "./pages/Report";
+import Home from "./pages/Home";
 
 export default function App() {
   const [page, setPage] = useState("home");
